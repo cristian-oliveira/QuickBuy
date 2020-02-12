5e5a7f1d69729c7c11fe747bd9c97a25ba55f634
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-12 17:29:22 -0300

    configurando conttexto do banco de dados no startup

--- QuicBuy.web/Startup.cs
+++ QuicBuy.web/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuickBuy.repositorio.Contexto;
 
 namespace QuicBuy.web
 {
@@ -12,7 +14,10 @@ namespace QuicBuy.web
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json",optional:false,reloadOnChange: true);
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -20,6 +25,9 @@ namespace QuicBuy.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<QuickBuyContexto>(option => option.UseMySql(connectionString,
+                                                                                   m => m.MigrationsAssembly("QuickBuy.repositorio")));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
