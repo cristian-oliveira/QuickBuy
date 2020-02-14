752f5d12e256c176549065e9f1ef77c290ae8c01
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-14 13:24:29 -0300

    Delete do Cliente Repositorio

--- QuickBuy.repositorio/Repositorios/ClientRepositorio.cs
+++ QuickBuy.repositorio/Repositorios/ClientRepositorio.cs
@@ -1,17 +1,1 @@
-﻿using QuickBuy.dominio.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace QuickBuy.repositorio.Repositorios
-{
-    public class ClientRepositorio
-    {
-        public ClientRepositorio()
-        {
-            var usuarioRepositorio = new UsuarioRepositorio();
-            var produto = new Produto();
-            usuarioRepositorio.Adicionar(produto);
-        }
-    }
-}
+﻿
