f843959a2b19cfb15656cae6a335b874462bf913
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-14 12:30:59 -0300

    Adicionando rotas do controller para front

--- QuicBuy.web/Controllers/ProdutoController.cs
+++ QuicBuy.web/Controllers/ProdutoController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using QuickBuy.dominio.Contratos;
+using QuickBuy.dominio.Entidades;
 using QuickBuy.repositorio.Repositorios;
+using System;
 using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
 
 namespace QuicBuy.web.Controllers
@@ -13,10 +15,34 @@ namespace QuicBuy.web.Controllers
 
         public ProdutoController(IProdutoRepositorio produtoRepositorio)
         {
-            //_produtoRepositorio = new ProdutoRepositorio(new QuickBuy.repositorio.Contexto.QuickBuyContexto()); 
-           _produtoRepositorio = produtoRepositorio;
+           // _produtoRepositorio = new ProdutoRepositorio(new QuickBuy.repositorio.Contexto.QuickBuyContexto());
+            _produtoRepositorio = produtoRepositorio;
+        }
 
-
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_produtoRepositorio.ObterTodos()); // Se for ok ele retorna os  produtos cadastrados na base de dados 
+                //if (condicao == false)
+                //    return BadRequest("");
+            }catch (Exception ex){
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)// transforma a informacao vinda de json em objeto conhecido
+        {
+            try
+            {
+                //produto.Nome = "";
+                _produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto); // retorna se conseguiu criar produto
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
