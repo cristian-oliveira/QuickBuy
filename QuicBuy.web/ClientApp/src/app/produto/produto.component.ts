d0670ad10c9ae38638b8d33bd368ca61bbc59439
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-17 18:36:11 -0300

    importacao de produtocomponent e criacao do .html

--- QuicBuy.web/ClientApp/src/app/produto/produto.component.ts
+++ QuicBuy.web/ClientApp/src/app/produto/produto.component.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core" //importacao do componente
 
 @Component({ // fazer com que o produto funcione como componente
   selector: "produto", //onde o componente sera renderizado
-  template:"<html><body>{{ obterNome() }}</body></html>"
+  templateUrl: '/produto.component.html'
 })
-export class ProdutoCompenent { //semelhante ao public no c#
+export class ProdutoCompenent { //semelhante ao public no c#; precisa ter export para que seja possivel realizar a importacao em app.module
   nome: string;
   public liberadoParaVenda: boolean;
 
