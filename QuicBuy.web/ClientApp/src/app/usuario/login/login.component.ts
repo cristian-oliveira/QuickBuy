ea63b43d084bb1ab4976ae39006aafb8847d9376
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-18 13:09:39 -0300

    estilizacao e binding tela login

--- QuicBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuicBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component} from "@angular/core";
+import { Component} from "@angular/core";
 
 @Component ({
     selector: "app-login",
@@ -7,5 +7,13 @@
 })
 
 export class LoginComponent{
-
+  public email = "";
+  //public endImg = "C:/Users/Dell/Desktop/Nova pasta/QuickBuy/QuicBuy.web/ClientApp/src/assets/imgs/banner-best-buy-260nw-713290624.jpg";
+  //public titulo = "Titulo adicionado";
+  entrar() {
+    alert(this.email);
+  }
 }
+
+
+//IMAGEM PARA FUNDO: https://img.olhardigital.com.br/uploads/acervo_imagens/2019/03/r16x9/20190315121922_1200_675_-_compras_online_e_commerce.jpg
