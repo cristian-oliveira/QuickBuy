437b93aeb3b7f786386d3982bbcdcfe228847731
Cristian Luiz dos Santos Oliveira <cristian.oliveira@dtidigital.com.br>
2020-02-18 17:27:09 -0300

    teste de validacao email ngIf

--- QuicBuy.web/ClientApp/src/app/app.module.ts
+++ QuicBuy.web/ClientApp/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoCompenent } from './produto/produto.component';
 import { LoginComponent } from './usuario/login/login.component';
+import { GuardaRotas } from "./autorizacao/GuardaRotas";
 
 
 @NgModule({
@@ -31,7 +32,7 @@ import { LoginComponent } from './usuario/login/login.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'produto', component: ProdutoCompenent },
+      { path: 'produto', component: ProdutoCompenent, canActivate:[GuardaRotas] },
       { path: 'entrar', component: LoginComponent }
 
     ])
